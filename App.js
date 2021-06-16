afc032c2c1cf560ae73d7c758ac2887f9fa53621
ysb94 <dbstjfqls199@naver.com>
2021-06-17 01:33:11 +0900

    06170133

--- App.js
+++ App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Alert } from "react-native";
 import Loading from "./Loading";
+import * as Location from "expo-location";
 
-export default function App() {
-  return <Loading />;
+export default class extends React.Component {
+  getLocation = async () => {
+    try {
+      const response = await Location.requestForegroundPermissionsAsync();
+      console.log(response);
+      const location = await Location.getCurrentPositionAsync();
+      console.log(location);  
+    } catch (error) {
+      Alert.alert("Can't find you.", "so sad");
+    }
+  };
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    return <Loading />;
+  }
 }
 
