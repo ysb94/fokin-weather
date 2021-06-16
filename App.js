25ef4c1677ea3828d46f4ebce4cba534d9d03d65
ysb94 <dbstjfqls199@naver.com>
2021-06-16 18:51:44 +0900

    06161851

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hhhh</Text>
+      <Text>12234</Text>
       <StatusBar style="auto" />
     </View>
   );
